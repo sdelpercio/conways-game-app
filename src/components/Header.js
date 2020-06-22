2b32bf81f284fe6ab17657ba6eb968310c02b394
Shawn DelPercio <sdelperc@gmail.com>
2020-06-22 13:32:00 -0400

    created basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
+// styles
+import { GiSprout } from 'react-icons/gi'
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+  width: 100%;
+  border-bottom: 1px solid grey;
+  display: flex;
+  justify-content: center;
+`
 
 function Header() {
   return (
-    <div>
-      <div>Conways game</div>
-    </div>
-  );
+    <StyledHeader>
+      <h1>
+        <GiSprout /> Conway's Game of Life
+      </h1>
+    </StyledHeader>
+  )
 }
 
-export default Header;
+export default Header
