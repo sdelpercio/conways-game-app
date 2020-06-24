448794684881c01ff4d98a220e7e1be5c687be8d
Shawn DelPercio <sdelperc@gmail.com>
2020-06-24 13:21:28 -0400

    implemented simulateAutomata function

--- src/helpers/simulate.js
+++ src/helpers/simulate.js
@@ -276,28 +276,17 @@ export const checkAlive = (cell, index, grid, size) => {
   }
 }
 
-// for each of the neighbors add one to counter if alive
-
-// if current cell is alive and counter is 2 or 3
-//      current cell remains alive (return true)
-// else
-//      current cell dies (return false)
-
-// if current cell is dead and counter is 3
-//      current cell lives (return true)
-// else
-//      current cell remains dead (return false)
-
-//
-//
-//
-
 // function to loop through entire board to check for updates
 // take in the nextGrid ref to do changes
+export const simulateAutomata = (grid, size) => {
+  // loop through nextGrid
+  grid.forEach((cell, index) => {
+    if (checkAlive(cell, index, grid, size)) {
+      cell.alive = true
+    } else {
+      cell.alive = false
+    }
+  })
 
-// loop through nextGrid
-//      for each cell, use check cell function
-//      if return is true, cell.alive = true
-//      else, cell.alive = false
-
-// return nextGrid
+  return grid
+}
